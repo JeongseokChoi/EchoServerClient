2b4278db5bd9b6ecf9dfc3a1ed9855e821303dfb
Jeong-seok Choi <jeongseok_choi@hanmail.net>
2013-02-17 23:56:17 +0900

    2013-02-17

---
+++ EchoClient.py
@@ -0,0 +1,11 @@
+from socket import *
+
+HOST = '127.0.0.1'
+PORT = 12328
+
+sock = socket(AF_INET, SOCK_STREAM)
+sock.connect((HOST, PORT))
+sock.sendall(b'Hello World!')
+data = sock.recv(1024)
+sock.close()
+print('Received', repr(data.decode()))
