359069cf944647086855689ad89bf32034fcba44
Jeong-seok Choi <jeongseok_choi@hanmail.net>
2013-02-19 01:48:57 +0900

    2013-02-19

--- EchoClient.py
+++ EchoClient.py
@@ -1,11 +1,31 @@
+#-*- encode: cp949 -*-
+
+
+# < 클라이언트 프로그램의 흐름 >
+#
+#       socket()      : 소켓을 생성한다.
+#          |
+#       connect()     : 서버에 연결한다.
+#          |
+#   send() & recv()   : 데이터를 주고받는다.
+
 from socket import *
 
 HOST = '127.0.0.1'
 PORT = 12328
 
+# 소켓을 생성한다.
+# 인터넷(AF_INET)을 사용하고, TCP프로토콜(SOCK_STREAM)을 사용한다.
 sock = socket(AF_INET, SOCK_STREAM)
+
+# 서버에 연결한다.
+# 입력한 주소(HOST)로 입력한 포트(PORT)를 통해 접속한다.
 sock.connect((HOST, PORT))
+
+# 데이터를 주고받는다.
 sock.sendall(b'Hello World!')
 data = sock.recv(1024)
-sock.close()
+
 print('Received', repr(data.decode()))
+
+sock.close()
--- EchoServer.py
+++ EchoServer.py
@@ -1,15 +1,47 @@
+#-*- encode: cp949 -*-
+
+
+# < 서버 프로그램의 흐름 >
+#
+#       socket()      : 소켓을 생성한다.
+#          |
+#        bind()       : 소켓에 정보를 입력한다.
+#          |
+#       listen()      : 소켓을 연결 대기 상태로 만든다.
+#          |
+#       accept()      : 클라이언트의 연결 요청을 받아들인다.
+#          |
+#   send() & recv()   : 데이터를 주고받는다.
+
+
 from socket import *
+
 
 HOST = ''
 PORT = 12328
 
+
+# 소켓을 생성한다.
+# 인터넷(AF_INET)을 사용하고, TCP프로토콜(SOCK_STREAM)을 사용한다.
 server_sock = socket(AF_INET, SOCK_STREAM)
+
+# 소켓에 정보(주소와 포트)를 입력한다.
 server_sock.bind((HOST, PORT))
+
+# 소켓을 연결 대기 상태로 만든다.
+# 최대 5개까지 대기열에 넣는다.
 server_sock.listen(5)
+
+# 클라이언트의 연결 요청을 받아들인다.
 conn, addr = server_sock.accept()
+
 print('Connected by', addr)
+
+# 데이터를 주고받는다.
 while True:
     data = conn.recv(1024)
-    if not data: break
+    if not data:
+        break
     conn.sendall(data)
+
 conn.close()
